54f230b519b3de8a2c0f2ed5cc715d6a4c950fac
ibrahim 6leaves <ibrahim6yaprak@gmail.com>
2022-11-12 00:29:49 -0500

    NewSearch

---
+++ src/Day04/GoogleNewSearch.java
@@ -0,0 +1,17 @@
+package Day04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleNewSearch {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        driver.findElement(By.name("q")).sendKeys("new search");
+        driver.findElement(By.tagName("Search")).click();
+
+    }
+}
