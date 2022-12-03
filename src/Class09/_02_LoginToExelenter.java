a4439dad1ff8e89b7157c3ac2f6bf56f358c9c17
ibrahim 6leaves <ibrahim6yaprak@gmail.com>
2022-12-02 22:03:03 -0500

    working with properties file

---
+++ src/Class09/_02_LoginToExelenter.java
@@ -0,0 +1,56 @@
+package Class09;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class _02_LoginToExelenter {
+    public static WebDriver driver;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String filePath = System.getProperty("user.dir") + "/configs/config.properties";
+        FileInputStream fis = new FileInputStream(filePath);
+        Properties properties = new Properties();
+        properties.load(fis);
+
+        String browser = properties.getProperty("browser");
+
+        //new style switch
+        switch (browser) {
+            case "chrome" -> {
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+                driver = new ChromeDriver();
+            }
+            case "firefox" -> {
+                System.setProperty("webdriver.geckodriver.driver", "drivers/gecko.exe");
+                driver = new FirefoxDriver();
+            }
+            case "edge" -> {
+                System.setProperty("webdriver.msedgedriver.driver", "drivers/msedgedriver.exe");
+                driver = new EdgeDriver();
+            }
+        }
+
+        //old style
+//        switch (browser){
+//            case "chrome" :
+//                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//                driver = new ChromeDriver();break;
+//            case "firefox" :
+//                System.setProperty("webdriver.geckodriver.driver","drivers/gecko.exe");
+//                driver = new FirefoxDriver();break;
+//            case "edge" :
+//                System.setProperty("webdriver.msedgedriver.driver","drivers/msedgedriver.exe");
+//                driver = new EdgeDriver();break;
+//        }
+
+        driver.get(properties.getProperty("url"));
+
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
---
+++ src/Class10/_01_Excell_Intro.java
@@ -0,0 +1,7 @@
+package Class10;
+
+public class _01_Excell_Intro {
+    public static void main(String[] args) {
+
+    }
+}
